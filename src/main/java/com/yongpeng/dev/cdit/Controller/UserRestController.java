d81b48f33d098e1ca0e1db6c7286e03b72a33a6b
local user <localuser@INLLYPE-150512.IRAS-PROD.SG>
2018-12-23 14:11:41 +0800

    creating users via Rest API

--- src/main/java/com/yongpeng/dev/cdit/Controller/UserRestController.java
+++ src/main/java/com/yongpeng/dev/cdit/Controller/UserRestController.java
@@ -11,6 +11,11 @@ import com.yongpeng.dev.cdit.DTO.ResultsDTO;
 import com.yongpeng.dev.cdit.DTO.ListResultsDTO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.xml.ws.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserRestController {
@@ -18,19 +23,43 @@ public class UserRestController {
   @Autowired
   private UserService userService;
 
-    @RequestMapping("/users")
-    public ResultsDTO<UserDTO> users() {
-      ResultsDTO<UserDTO> results = new ListResultsDTO<UserDTO>(userService.findByRequiredRange());
-      return results;
-    }
+  @RequestMapping("/users")
+  public ResultsDTO<UserDTO> users() {
+    ResultsDTO<UserDTO> results = new ListResultsDTO<UserDTO>(userService.findByRequiredRange());
+    return results;
+  }
 
-    @RequestMapping(method = RequestMethod.POST, value="/create/user")
-    public ResponseEntity<String> CreateUser(@RequestBody UserDTO user){
-      if(userService.IsValid(user))
+  @RequestMapping(method = RequestMethod.POST, value="/create/user")
+  public ResponseEntity<String> CreateUser(@RequestBody UserDTO user){
+    if(userService.IsValid(user))
+    {
+      userService.createUser(user);
+      return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value="/create/users")
+  public ResponseEntity<String> CreateUser(@RequestBody List<UserDTO> users ){
+    for(UserDTO user: users){
+      if(!userService.IsValid(user))
       {
-        userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
       }
-      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
     }
+
+    for(UserDTO user: users) {
+      userService.createUser(user);
+    }
+    return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+  }
+  @RequestMapping("/test")
+  public void test(){
+    String uri = "http://localhost:8080/create/users";
+    List<UserDTO> list = new ArrayList<UserDTO>();
+    UserDTO user = new UserDTO("Alan",1000.0);
+    list.add(user);
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.postForObject(uri,list, ResponseEntity.class);
+  }
 }
